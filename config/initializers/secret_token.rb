9b5ad46da13b8c7842e25f0601c40774b18bca84
kgong <kevinkgong@gmail.com>
2014-02-11 02:37:59 -0800

    Initial Rails App + New Relic

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Rtr4c::Application.config.secret_key_base = 'b8e8fb189b71e0a6cbee16e415a2b4ba9faf43d559d2998a0595233df0b6ce6e5710db6872caa426f1de18bb3773490d7aeccf0616ccc610bdb2b7eafccc423c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Rtr4c::Application.config.session_store :cookie_store, key: '_rtr4c_session'
