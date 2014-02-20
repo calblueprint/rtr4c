8c708564df933a6c01fbf02cacaf77d35908f006
maobster <maobster@airbears2-10-142-37-7.airbears2.1918.berkeley.edu>
2014-02-19 20:06:01 -0800

    add paperclip defaults to production and development environments

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,4 +26,13 @@ Rtr4c::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+
+  config.paperclip_defaults = {
+    :storage => :s3,
+    :s3_credentials => {
+      :bucket => ENV['S3_BUCKET'],
+      :access_key_id => ENV['S3_KEY'],
+      :secret_access_key => ENV['S3_SECRET']
+    }
+  }
 end
