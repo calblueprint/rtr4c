2d43ef0197d498a97889b07ce7771eec74ff837d
maobster <maobster@airbears2-10-142-37-7.airbears2.1918.berkeley.edu>
2014-02-19 20:14:35 -0800

    set products as a resource in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rtr4c::Application.routes.draw do
   root 'welcome#index'
+
+  resources :products
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
