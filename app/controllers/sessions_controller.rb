1ed4c6f499d4839695c74eee774b27deefdff49d
maobster <youj3an@gmail.com>
2014-03-04 19:27:57 -0800

    create prettier routes for login/logout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,17 +3,17 @@ class SessionsController < ApplicationController
   end
 
   def create
-  	user = User.find_by_email(params[:email])
-  	if (user && user.authenticate(params[:password])
-  		session[:user_id] = user.id
-  		redirect_to root_url, notice: "Logged in!"
-  	else
-  		flash.now.alert = "Email or password is invalid"
-  	end
+    user = User.find_by_email(params[:email])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to root_url, notice: "Logged in!"
+    else
+      flash.now.alert = "Email or password is invalid."
+    end
   end
 
   def destroy
-    dession[:user_id] = nil
+    session[:user_id] = nil
     redirect_to root_url, notice: "Logged out!"
   end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,10 +1,5 @@
 class Product < ActiveRecord::Base
-<<<<<<< HEAD
-	has_attached_file :photo
-	validates_attachment_content_type :photo, :content_type => /\Aimage\/.*\Z/
-=======
   has_attached_file :photo
   
   validates_attachment_content_type :photo, :content_type => /\Aimage\/.*\Z/
->>>>>>> c6aa221a80cef594c18c9f85464a180bc2f440f6
 end
