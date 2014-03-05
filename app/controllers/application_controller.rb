c84a79b7e0745b400734077ab44b93195e7912ca
maobster <youj3an@gmail.com>
2014-03-05 00:07:05 -0800

    limit product actions to authenticated users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,12 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  private 
+  
+def authorize
+  redirect_to login_url, alert: "Not authorized" if current_user.nil?
+end
+
+private 
   def current_user
   	@current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,6 @@
 class ProductsController < ApplicationController
   before_action :set_product, only: [:show, :edit, :update, :destroy]
+  before_action :authorize, only: [:edit, :update]
 
   # GET /products
   # GET /products.json
