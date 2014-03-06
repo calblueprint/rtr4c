5098b92637f393cf6ddeb25f3671522b1aaaeb86
maobster <youj3an@gmail.com>
2014-03-05 19:12:05 -0800

    add user account to seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.create(email:"happydays@happydays.com", password: "happy", password_confirmation:"happy");
