11c879a5fe05547b3dd8df29e7d5049da1269e66
maobster <youj3an@gmail.com>
2014-03-05 19:41:14 -0800

    simplify seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,4 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-User.create(email:"happydays@happydays.com", password: "happy", password_confirmation:"happy");
+User.create(email:"happy", password: "happy", password_confirmation:"happy");
