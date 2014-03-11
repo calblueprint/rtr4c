8bd2c1c0516533576779df5a9dfd233beaa55195
kgong <kevinkgong@gmail.com>
2014-03-10 20:06:05 -0700

    oops, mangled the merge

--- db/schema.rb
+++ db/schema.rb
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 20140310234300) do
     t.string   "profile"
     t.datetime "created_at"
     t.datetime "updated_at"
+  end
 
   create_table "images", force: true do |t|
     t.string   "name"
