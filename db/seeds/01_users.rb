87e531ce0b4b71c17c0831fc547dbb43015fc775
kgong <kevinkgong@gmail.com>
2014-03-17 15:19:04 -0700

    split seeds.rb into multiple seeding files in db/seeds

---
+++ db/seeds/01_users.rb
@@ -0,0 +1,6 @@
+######################### Single User Authentication ##########################
+User.create(email: "happy", 
+            password: "happy", 
+            password_confirmation: "happy");
+
+###############################################################################
