a660cf7fd2cefa02e3dc05662687eb68d4938853
kgong <kevinkgong@gmail.com>
2014-03-17 16:12:23 -0700

    fix donations and add press updates

--- db/seeds/02_donations_and_donors.rb
+++ db/seeds/02_donations_and_donors.rb
@@ -1,10 +1,9 @@
 ###################### Donors and associated donations ########################
-Donor.create(   email: "kindsir@kindness.com",
-                name: "William Robinson",
-                title: "CFO of GreatMoves",
-                profile: "http://www.linkedin.com/",
-                amount: 0.0)
-donor = Donor.find_by email: "kindsir@kindness.com"
+donor = Donor.create(   email: "kindsir@kindness.com",
+                        name: "William Robinson",
+                        title: "CFO of GreatMoves",
+                        profile: "http://www.linkedin.com/",
+                        amount: 0.0)
 
 Donation.create(amount: 888.88, 
                 message: "Love what RTR4C does. I hope I can make it into"\
@@ -12,12 +11,11 @@ Donation.create(amount: 888.88,
                 donor_id: donor.id)
 
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
-Donor.create(   email: "kanye@kanyewest.edu",
-                name: "Kanye West",
-                title: "Monster",
-                profile: "http://www.kanyewest.com/",
-                amount: 0.0)
-donor = Donor.find_by email: "kanye@kanyewest.edu"
+donor = Donor.create(   email: "kanye@kanyewest.edu",
+                        name: "Kanye West",
+                        title: "Monster",
+                        profile: "http://www.kanyewest.com/",
+                        amount: 0.0)
 
 Donation.create(amount: 444.44, 
                 message: "Because silver is the closest of the tiers to the "\
@@ -25,22 +23,20 @@ Donation.create(amount: 444.44,
                 donor_id: donor.id)
 
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
-Donor.create(   email: "callmemc@gmail.com",
-                name: "Michelle Chow",
-                title: "Software Engineer",
-                amount: 0.0)
-donor = Donor.find_by email: "callmemc@gmail.com"
+donor = Donor.create(   email: "callmemc@gmail.com",
+                        name: "Michelle Chow",
+                        title: "Software Engineer",
+                        amount: 0.0)
 
 Donation.create(amount: 100.00, 
                 message: "I wish people would actually call me mc...",
                 donor_id: donor.id)
 
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
-Donor.create(   email: "",
-                name: "Michelle Chow",
-                title: "Software Engineer",
-                amount: 0.0)
-donor = Donor.find_by email: "callmemc@gmail.com"
+donor = Donor.create(   email: "growingman@growth.com",
+                        name: "John Grow",
+                        title: "Aspiring All-Star",
+                        amount: 0.0)
 
 Donation.create(amount: 14.00, 
                 message: "All I can afford to give now, but I hope I can do "\
---
+++ db/seeds/03_press_updates.rb
@@ -0,0 +1,172 @@
+############################### Press Updates #################################
+update = PressUpdate.create( 
+    title: "RTR4C featured on Cincy Chic",
+    link:  "http://cincychic.com/index.php?option=com_content&view=article"\
+           "&id=5014:rooms-that-rock-4-chemo&catid=15:health&Itemid=10882")
+date_time = DateTime.new(2013,11,17)
+update.created_at = date_time
+update.updated_at = date_time
+update.save
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+update = PressUpdate.create( 
+    title: "Wood + Candy = Room that Rocks Sweetly",
+    link:  "http://www.ariahealth.org/default.aspx?pageid=6849")
+date_time = DateTime.new(2013,11,17)
+update.created_at = date_time
+update.updated_at = date_time
+update.save
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+update = PressUpdate.create( 
+    title: "Photo at 2012 City and County of San Francisco Combined"\
+           "Charities Campaign",
+    link:  "https://www.facebook.com/photo.php?fbid=10151150092047144"\
+           "&set=o.341818389241454&type=1")
+date_time = DateTime.new(2012,10,04)
+update.created_at = date_time
+update.updated_at = date_time
+update.save
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+update = PressUpdate.create( 
+    title: "Camden Clark Medical Center shows new chemotherapy rooms",
+    link:  "http://www.newsandsentinel.com/page/content.detail/id/565842/"\
+           "CCMC-shows-new-chemotherapy-rooms.html?nav=5061")
+date_time = DateTime.new(2012,10,01)
+update.created_at = date_time
+update.updated_at = date_time
+update.save
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+update = PressUpdate.create( 
+    title: "National Maternity Hospital in El Salvador Receives Makeover of "\
+           "Oncology Ward",
+    link:  "http://www.prweb.com/releases/2012/9/prweb9858039.htm")
+date_time = DateTime.new(2012,9,20)
+update.created_at = date_time
+update.updated_at = date_time
+update.save
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+update = PressUpdate.create( 
+    title: "El Salvador's National Maternity Hospital",
+    link:  "http://www.prweb.com/releases/2012/7/prweb9618516.htm"\
+           "#.UA8VJCiNQAk.email")
+date_time = DateTime.new(2012,07,24)
+update.created_at = date_time
+update.updated_at = date_time
+update.save
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+update = PressUpdate.create( 
+    title: "",
+    link:  "")
+date_time = DateTime.new(2012,10,01)
+update.created_at = date_time
+update.updated_at = date_time
+update.save
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+update = PressUpdate.create( 
+    title: "RTR4C Brings Interior Designers Together To Brighten Spaces "\
+           "For Those Fighting Cancer-AskMissA.com",
+    link:  "http://askmissa.com/2011/07/12/san-franciscos-"\
+           "roomsthatrock4chemo-brings-interior-designer-together-to-brighten"\
+           "-spaces-for-those-fighting-cancer/")
+date_time = DateTime.new(2011,07,12)
+update.created_at = date_time
+update.updated_at = date_time
+update.save
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+update = PressUpdate.create( 
+    title: "Chemo room getting 'spa' look - NewandSentinel.com",
+    link:  "http://www.newsandsentinel.com/page/content.detail/id/561449/"\
+           "Chemo-room-getting--spa--look.html?nav=5055")
+date_time = DateTime.new(2012,05,26)
+update.created_at = date_time
+update.updated_at = date_time
+update.save
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+update = PressUpdate.create( 
+    title: "Chemotherapy Center Grand Opening at Mammoth Hospital",
+    link:  "http://mammothtimes.com/content/chemotherapy-center-grand-opening"\
+           "-mammoth-hospital")
+date_time = DateTime.new(2013,01,04)
+update.created_at = date_time
+update.updated_at = date_time
+update.save
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+update = PressUpdate.create( 
+    title: "Marin General Hospital Rocks Its Chemo Suites",
+    link:  "http://novato.patch.com/groups/announcements/p/an--marin-general-"\
+           "hospital-rocks-its-chemo-suites-28327bc7")
+date_time = DateTime.new(2012,12,28)
+update.created_at = date_time
+update.updated_at = date_time
+update.save
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+update = PressUpdate.create( 
+    title: "Cora Sue Anthony of HGTV Gives Praise",
+    link:  "http://www.californiahomedesign.com/gallery/designer-crush-qa-"\
+           "cora-sue-anthony-hgtvs-real-estate-intervention#1")
+date_time = DateTime.new(2013,05,07)
+update.created_at = date_time
+update.updated_at = date_time
+update.save
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+update = PressUpdate.create( 
+    title: "RTR4C featured in the San Francisco Chronicle",
+    link:  "http://www.sfchronicle.com/health/article/Rooms-That-Rock-4-"\
+           "Chemo-adds-cheer-4477454.php#ixzz2SAGFaggX")
+date_time = DateTime.new(2013,04,30)
+update.created_at = date_time
+update.updated_at = date_time
+update.save
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+update = PressUpdate.create( 
+    title: "Mountain View's El Camino Hospital Cancer Center's brighter "\
+           "walls help patients heal",
+    link:  "http://www.mercurynews.com/breaking-news/ci_23799133/brighter-"\
+           "hospitals-walls-boost-cancer-patients")
+date_time = DateTime.new(2013,08,05)
+update.created_at = date_time
+update.updated_at = date_time
+update.save
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+update = PressUpdate.create( 
+    title: "Cutting Edge Stencils Help Brighten Rooms That Rock 4 Chemo!",
+    link:  "http://blog.cuttingedgestencils.com/stenciling-rooms-that-rock-"\
+           "4-chemo.html")
+date_time = DateTime.new(2013,11,14)
+update.created_at = date_time
+update.updated_at = date_time
+update.save
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+update = PressUpdate.create( 
+    title: "Featured on Upside CNN",
+    link:  "https://www.facebook.com/photo.php?v=544682168915103&saved")
+date_time = DateTime.new(2013,08,09)
+update.created_at = date_time
+update.updated_at = date_time
+update.save
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+update = PressUpdate.create( 
+    title: "KTVU News - BAY AREA PEOPLE: Rooms That Rock 4 Chemo",
+    link:  "http://www.ktvu.com/videos/news/bay-area-people-rooms-that-rock-"\
+           "4-chemo/v2zXx/")
+date_time = DateTime.new(2013,11,14)
+update.created_at = date_time
+update.updated_at = date_time
+update.save
+
+###############################################################################
