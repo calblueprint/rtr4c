9efd734e0333babd05ed1e2a0c17f5fff748be4a
kgong <kevinkgong@gmail.com>
2014-03-19 20:14:01 -0700

    simple product seed data

---
+++ db/seeds/05_products.rb
@@ -0,0 +1,20 @@
+################################## Products ###################################
+Product.create( 
+  name:         "Shirt",
+  description:  "This is an awesome shirt!",
+  price:        "10.00")
+
+Product.create( 
+  name:         "Pen",
+  description:  "This is an awesome pen!",
+  price:        "1.00")
+
+Product.create( 
+  name:         "Mark Miyashita",
+  description:  "He puts the -shit- in Miyashita",
+  price:        "0.69")
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+
+###############################################################################
