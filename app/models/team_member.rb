dc01661803226d25fb4c4ef534fecbdc68855c7c
Eugene Kim <maobster@ubuntu.(none)>
2014-03-29 14:01:36 -0700

    create TeamMember scaffold

--- app/models/team_member.rb
+++ app/models/team_member.rb
@@ -1,2 +1,4 @@
 class TeamMember < ActiveRecord::Base
+  has_many :images, as: :imageable # keep has_many just for now; nested forms bugs out
+  accepts_nested_attributes_for :images, allow_destroy: true
 end
