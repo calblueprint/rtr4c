1f1b8dadeb9024f3176206243a8823af4fe7d0b9
maobster <youj3an@gmail.com>
2014-04-09 18:29:58 -0700

    create image url helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  # returns url of first image associated with the model instance
+  def first_image(instance)
+    if instance.images.size > 0
+      instance.images[0].photo.url
+    end
+  end
 end
