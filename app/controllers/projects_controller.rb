c829ac714e8c07afb197d11f79da32e27b314b15
maobster <youj3an@gmail.com>
2014-04-13 16:46:11 -0700

    remove unnecessary formatting options in projects_controller

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -25,28 +25,20 @@ class ProjectsController < ApplicationController
   # POST /projects.json
   def create
     @project = Project.new(project_params)
-
-    respond_to do |format|
-      if @project.save
-        format.html { redirect_to @project, notice: 'Project was successfully created.' }
-        format.json { render action: 'show', status: :created, location: @project }
-      else
-        format.html { render action: 'new' }
-        format.json { render json: @project.errors, status: :unprocessable_entity }
-      end
+    if @project.save
+      redirect_to @project, notice: 'Project was successfully created.'
+    else
+      render action: 'new'
     end
   end
 
   # PATCH/PUT /projects/1
   # PATCH/PUT /projects/1.json
   def update
-    respond_to do |format|
       if @project.update(project_params)
-        format.html { redirect_to @project, notice: 'Project was successfully updated.' }
-        format.json { head :no_content }
+        redirect_to @project, notice: 'Project was successfully updated.'
       else
-        format.html { render action: 'edit' }
-        format.json { render json: @project.errors, status: :unprocessable_entity }
+        render action: 'edit'
       end
     end
   end
@@ -55,9 +47,7 @@ class ProjectsController < ApplicationController
   # DELETE /projects/1.json
   def destroy
     @project.destroy
-    respond_to do |format|
-      format.html { redirect_to projects_url }
-      format.json { head :no_content }
+    format.html { redirect_to projects_url }
     end
   end
 
