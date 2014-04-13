02d583c85dc0fdb20b502611f2bf9184a788da28
maobster <youj3an@gmail.com>
2014-04-13 16:33:58 -0700

    add images to projects

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140404011326) do
+ActiveRecord::Schema.define(version: 20140413232612) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -80,6 +80,15 @@ ActiveRecord::Schema.define(version: 20140404011326) do
     t.string   "name"
     t.text     "description"
     t.decimal  "price"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "projects", force: true do |t|
+    t.string   "title"
+    t.datetime "date"
+    t.text     "description"
+    t.string   "location"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
