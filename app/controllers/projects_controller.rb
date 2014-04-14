6028a853b1db020ef386206609758f796eac2d61
maobster <youj3an@gmail.com>
2014-04-13 17:51:38 -0700

    add authorization requirements for projects_controller actions

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,5 +1,6 @@
 class ProjectsController < ApplicationController
   before_action :set_project, only: [:show, :edit, :update, :destroy]
+  before_action :authorize, except: [:index, :show]
 
   # GET /projects
   # GET /projects.json
