c35586b63914b4e9d2f30808c74a237fe0e06537
Hao Guan <haohowardguan@gmail.com>
2014-04-21 21:47:47 -0700

    news page has been integrated with backend model

--- app/models/press_update.rb
+++ app/models/press_update.rb
@@ -1,4 +1,9 @@
 class PressUpdate < ActiveRecord::Base
 	validates :title, presence: true
 	validates :link, presence: true
+
+
+  def get_embed_link
+    self.videolink.split('=').last if self.videolink
+  end
 end
