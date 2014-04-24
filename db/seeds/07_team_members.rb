ab0d513aaa9a988e9b752e10be9e9c7cfe0f154c
maobster <youj3an@gmail.com>
2014-04-23 21:18:55 -0700

    add all head honcho seed data

--- db/seeds/07_team_members.rb
+++ db/seeds/07_team_members.rb
@@ -4,6 +4,62 @@ t = TeamMember.new(
   blurb: 'A botanical watercolor artist and published author of several books including her new romance novel, Invited Guests. She is committed and excited to be involved with RoomsThatRock4Chemo. Much of the gratitude goes to the designers and volunteers who give so much. “It is overwhelming to be a part of the grace and generosity of our volunteers."',
   role: 0,
 )
-
 t.images.new().photo_from_url("http://www.roomsthatrock4chemo.us/images/nancy-linebarger-ballard.jpg")
 t.save
+
+t = TeamMember.new(
+  name: 'Paul Chin',
+  title: 'PRESIDENT',
+  blurb: 'Born in 1949 and educated in San Francisco, Military service with 347th General Hospital M.A.S.H 1968.Paul joined the San Francisco Fire Department in 1974. He retired in 2006 as the San Francisco Fire Marshall. His volunteer work includes: international children’s program (CISV), taking and working with children from at least 10 different countries in a one-month summer camp-like program promoting ethnic and cultural awareness in Romania, Sweden, New Zealand and United States just to name a few in the last 10 years.',
+  role: 0,
+)
+t.images.new().photo_from_url("http://www.roomsthatrock4chemo.us/images/paul-chin.jpg")
+t.save
+
+t = TeamMember.new(
+  name: 'Dr. Kevin Knopf',
+  title: 'VICE PRESIDENT',
+  blurb: 'Dr. Knopf is a Hematologist/Oncologist practicing in San Francisco. A native of Long Beach, California he attended college at the Massachusetts Institute of Technology, medical school at University of California, San Francisco and did training in internal medicine and oncology at Northwestern Medical School, Johns Hopkins Medical School, Sloan-Kettering Cancer Center and the National Cancer Institute.
+
+He has practiced in San Francisco since 2006 - out of California Pacific Medical Center and St Lukes, where he is Director of Hematology/Oncology. He also has a smaller practice in Internal Medicine. Dr Knopf runs the Quality Oncology Initiative at California Pacific Medical Center and is also involved in Cancer Genetics and the teaching of Interns and Residents. Dr Knopf publishes articles on a variety of cancers and presents at National and International Meetings with a focus on Health Services Research (the study of how cancer care is delivered). Dr Knopf is thrilled to be part of Rooms that Rock 4 Chemo and proud to work in an office where patients can share in the great work that RTR4C does every day.',
+  role: 0,
+)
+t.images.new().photo_from_url("http://www.roomsthatrock4chemo.us/images/dr-knopf.jpg")
+t.save
+
+t = TeamMember.new(
+  name: 'Mario Ballard',
+  title: 'SECRETARY',
+  blurb: 'As a firefighter and manager with the San Francisco Fire Department for 33 years I am honored to be a part of RoomsThatRock4Chemo. During the last 17 years of my career, I managed a creative group of men and women responsible for seeing that most everything built in the City met the hundreds of local and national codes and standards. As a result of this work, after my retirement I opened Mario Ballard and Associates, a building and fire code consulting firm.
+
+Like the Fire Department, RoomsThatRock is a dedicated service oriented organization. Ready to lend a helping, compassionate hand in times of need, always dedicated to helping others. It is in this spirit of service to others, that I look forward to working with this great endeavor.',
+  role: 0,
+)
+t.images.new().photo_from_url("http://www.roomsthatrock4chemo.us/images/Mario-Ballard.jpg")
+t.save
+
+t = TeamMember.new(
+  name: 'Michael Shaw CPA',
+  title: 'TREASURER',
+  blurb: 'Michael Shaw is a CPA and partner at Mill Valley tax practice, Shaw + Shaw, LLP. He\'s also a seasoned musician with a passion for both guitar and piano. Michael enjoys working with all his non-profit clients, but he\'s especially honored to join the board of Rooms that Rock as cancer has recently touched his family and he is profoundly committed to the cause.',
+  role: 0,
+)
+t.images.new().photo_from_url("http://www.roomsthatrock4chemo.us/images/Michael-Shaw.jpg")
+t.save
+
+t = TeamMember.new(
+  name: 'Andrea Tytell',
+  title: 'ATTORNEY AT LAW CHIEF LEGAL COUNSEL',
+  blurb: 'Andrea Tytell has been a practicing attorney since 1993. She is well respected by judicial officers, colleagues and clients alike. Ms. Tytell brings a great deal of passion to her work while handling each case with dignity, common sense and significant legal acumen. Ms. Tytell\'s addition to the Advisory Board of RTR4C allows us to handle all legal matters in house thus insuring the largest percentage of donations are spent upon the stated spirit and purpose of our organization. Ms. Tytell lost her mother and grandmother to cancer. She is therefore committed on a personal level to make each cancer patient\'s journey a little sunnier and brighter than the dark, clinical and dismal experiences suffered by her loved ones. http://andreatytell.com/',
+  role: 0,
+)
+t.images.new().photo_from_url("http://www.roomsthatrock4chemo.us/images/andrea-tytell.jpeg")
+t.save
+
+t = TeamMember.new(
+  name: 'Shoshana Ungerleider, MD',
+  title: 'RTR4C AMBASSADOR BOARD FOR YOUNG PROFESSIONALS',
+  blurb: 'Shoshana Ungerleider, MD is originally from Eugene, Oregon. She has lived in the Bay area for the last four years and completed her residency training in San Francisco at California Pacific Medical Center. She currently works as a hospital-based internal medicine physician and is Chair of the RoomThatRock4Chemo Ambassador Board for young professionals.',
+  role: 0,
+)
+t.images.
