75239c17d5fade14b3c409393e9e16c27c47416a
Brian Wayne Liou <brian.liou91@gmail.com>
2014-04-26 16:34:50 -0700

    seeded projects

--- db/seeds/08_projects.rb
+++ db/seeds/08_projects.rb
@@ -1,8 +1,124 @@
-t = Project.new(
-  title: 'sample project',
-  description: 'blah blah what a cool project',
+########################################### Projects ############################################
+project = Project.new(
+  title: 'Mammoth Hospital',
+  description: 'Mammoth Hospital was in need of revamping a large chemotherapy community room that had 10 chairs to provide 13,000 patient visits per year. Our RTR4C Southern California Designer, Nicole Cavanaugh came to their aid with a full crew and all needed supplies to create a healing loving room with a FIREPLACE! They open their doors to all that need chemotherapy. It went from drab and average to fabulous, amazing, cozy and warm. A special thank you to Enterprise Car Rental who donated a 4WD/SUV for the snowy trip to Mammoth and to Erica Wells, Chemo Nurse Manager at Mammoth Hospital who added all these details to her already busy schedule. You all rock!',
   volunteers: 'No volunteers',
-  location: 'berkeley!'
+  location: 'Mammoth, CA'
 )
-t.images.new().photo_from_url("https://s3-us-west-1.amazonaws.com/rtr4c-dev/projects/Projects/Aria/pict_one.png")
-t.save
+project.images.new().photo_from_url("https://s3-us-west-1.amazonaws.com/rtr4c-dev/projects/Projects/Mammoth/pict_two.jpg")
+project.save
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+project = Project.new(
+  title: 'Aria Hospital Cancer Center',
+  description: 'Twenty-nine patient chairs were impacted by this project where we stenciled, wallpapered, painted and purchased new privacy curtains between the chairs, new lighting, new closet for blankets, new nurses station, art work and visitor chairs. 19,500 patient visits are impacted as well as 22 visitor chairs at 28,600 visitor visits per year. Four rooms total were designed by our very first “Celebrity Designer,” Cora Sue Anthony, and features two full walls of photographed decals. So soothing, lovely and hopeful. This project was hosted by The Beverly Fund, a non profit founded by a daughter who lost her mother to lung cancer as she struggled through treatment in this very facility.',
+  volunteers: 'Donations by Tracy Sistili, Founder The Beverly Fund,  ION Solutions, Rebecca C Knockemus, Dali Decals, Cutting Edget Stencils, Ann Marie Edwards, COO Alliance Cancer Specialists, Cora Sue Anthony (HGTV Designer), Andres Torres Fine Arts Photographer, Nate Wood, Video Producer: VisionLine Media, Amy Ocasio, Fine Artist/Founder Whimsical Spirits of Whim-en, Colorful Artwork by Amy O, Wes Coventry, Fine Work Carpenter, Home Depot, and all the wonderful local volunteers!',
+  location: 'Philadelphia, PA'
+)
+project.images.new().photo_from_url("https://s3-us-west-1.amazonaws.com/rtr4c-dev/projects/Projects/Aria/pict_one.png")
+project.save
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+project = Project.new(
+  title: 'Camden Clark Medical Center',
+  description: 'This facility of over 5000 square feet included 9 treatments rooms/ 6 exams rooms / 3 nurses stations / 4 bathrooms / 1 conference room / 2 waiting areas / 2 doctors offices. This project resulted in over 50,000 patient/family visits per year. Those using the rooms benefit by a healing, calm and lovely environment in which to receive treatment. They even received a one year donation of candy in each chemo room, with a promise to keep them fully stocked. Each room was hosted and facilitated by individual groups including Susan G. Komen West Virginia, Habitat for Humanity, and many local establishments. There are over 100 pieces of donated art on the halls walls and in each room.',
+  volunteers: 'Designer Tamy Brady, Amy Ocasio, Project Manager, Kim Couch Executive Director Camden Clark Foundation, RTR4C Rock Star Sponsors: BB&T Lighthouse Project, The Virginia Neptune Foundation, The Oakland Foundation, The Family of Daniel O. Martin, Family and Friends of Joel Stout, Family and Friends of Tim Moore, Paradise Grille, The John Patrick Albright Foundation, in memory of Joe, Patricia, and John Patrick Albright, and The Allen C. Spurgeon Family. Jan Dils & Chuck Hughes, J. P. Henry’s Restaurant, Bre Hughes in memory of her grandmother Dorothy V. Smith, and Susan Hill Richardson in memory of her mother Lucy W. Hill , Barbara J. Martino, in memory of the family of Carlton C. Woo.',
+  location: 'Parkersburg, West Virginia'
+)
+project.images.new().photo_from_url("https://s3-us-west-1.amazonaws.com/rtr4c-dev/projects/Projects/Camden/pict_one.jpg")
+project.save
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+project = Project.new(
+  title: 'Marin General Hospital',
+  description: 'This Project involved revamping the Outpatient Oncology Department at Marin General Hospital, impacting 4 private/semi private rooms, one large community chemo room, patient/family waiting area, halls ways and staff kitchen. This project included 10 patient chairs, 2 beds and 10 family/visitor chairs equaling 28,600 patient/family visits per year. Those using the rooms benefit by a healing, calm and lovely environment in which to receive treatment. This project completed by nine local designers, each adopted a room rocked it as only they could!      ',
+  volunteers: 'Designer: Marsha Heckman, beautifully painted by Decorative Artist David R. Campbell,David@RowanArt.com his wife Donnelle Campbell, volunteer Floyd Heckman. Art by Gary Kinson of Mill Valley gkinson@yahoo.com, high quality print donated by Skylard Images in Sebastopol, darren@skylarkimages.com the LOTUS decal donated by Walter Hawkins of Dali Decals marketing@dalidecals.com.',
+  location: 'Greenbrae, CA'
+)
+project.images.new().photo_from_url("https://s3-us-west-1.amazonaws.com/rtr4c-dev/projects/Projects/Marin_Hospital/pict_one.png")
+project.save
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+project = Project.new(
+  title: 'Maternity Hospital',
+  description: 'We transformed an entire wing of the Maternity Hospital in San Salvador including donated new flooring, windows, air conditioning, nurses station & kitchen, bathrooms and tile, new chemo chairs, stenciling, art work and furniture. This project consisted of 11 rooms / 27 chairs / 8 beds translating into 54,600 patient visits per year. This hospital is for women only and they are now able to be in a clean, peaceful environment, knowing that in any language, complete strangers care about the condition they are in even if they don\'t know them. We were joined by Basic Health International based out of New York who contributed to the project with helpful hands and donations.',
+  volunteers: 'Kimball Star, Designer, Project Manager, Taylor Watts, Mario Ballard, Regina Cabrero, Dr. Mauricio Maza, Basic Health Internationa Team consisting of  Lauren Ditzian, Mariam Cremer, Wendy Liberante, Elizabeth & Katerine Conlisk, David & Naomi Einhorn, Dra. Dalia Saravia, Dra. Karla Alfaro. Stencils by Cutting Edge Stencils.',
+  location: 'San Salvador, El Salvador'
+)
+project.images.new().photo_from_url("https://s3-us-west-1.amazonaws.com/rtr4c-dev/projects/Projects/Maternity_Hospital/pict_two.png")
+project.save
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+project = Project.new(
+  title: 'Cancer Institute of San Salvador',
+  description: 'This project in El Salvador encompassed 7 rooms / 8 chairs / 4 beds including a community chemotherapy room, an infusion room, doctors offices, and nurses stations/offices. It made an impact on 15,600 patients receiving treatment. This Clinic accepts the very poor as well as those that are able to pay. The rooms are now sparkling clean, pretty, soothing and hopeful to all that must endure the hours they spend here.',
+  volunteers: 'Nicole Facciuto, Desginer, Biaz Bazán fundador de la Liga Contra el cáncer de El Salvador. (Dr. Narciso Diaz Bazan founder Cancer League of El Salvador), Sol Mesa (Presidente Liga Contra el cáncer de EL Salvador) (President of the Board of Directors).Dr. Salvador Díaz Bazán (Head Doctor of The Institute and Liga Contra el cáncer de EL Salvador)  Sylvia Wilson de Sol,  Dr. Mauricio Maza, Sandra Sol, Mayu Monterrosa, Raquel Diaz Bazan, Alexandra Brugal, Beatriz Brugal, Alexandra Quintanilla, Alexandra Herrera, Marcela Gutierez, Sofia Bukele, Geraldina Cestoni, Ximena Cestoni, Rodrigo Escobar, Michael Levinson, Georgia Parker, Julie Taylor, Margaret Herron, Heidi Danos, MacMan, Marnie F. Aulabaugh, Emily Bunning, Christina Schierman,Dale Moon, Julie Maigret, Allaire Linebarger, Jon Levine, Sarah Logan, Courtney McGinnis, Nicole Mitchell. Stencils donated by Cutting Edge Stencils.',
+  location: 'El Salvador'
+)
+project.images.new().photo_from_url("https://s3-us-west-1.amazonaws.com/rtr4c-dev/projects/Projects/Cancer_Institute/room_two.jpg")
+project.save
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+project = Project.new(
+  title: 'Sequoia Hospital',
+  description: 'Revamped the MRI waiting room at Sequoia Hospital that included 9 chairs and facilitates 18,720 patient visits per year. Scope of work included fresh paint and stenciling, artwork, water and light treatments. This room is used by adults (18 and over) as they wait for MRI services that are provided to all. It is now more peaceful and calm not only to the patients but the staff as well. The kinder lighting eliminates the harsh hospital-like glare that is not necessary in a waiting room, and soft music is now piped in for all to enjoy.',
+  volunteers: 'Designer Jamie L. Belew. Darby Anderson of Jiill\'s Legacy, Michael Belew Photographer, Taylor Gill, Taylor Watts. Stencils donated by Cutting Edge Stencils.',
+  location: 'Redwood City, CA'
+)
+project.images.new().photo_from_url("https://s3-us-west-1.amazonaws.com/rtr4c-dev/projects/Projects/Sequoia_Hospital/pict_two.jpeg")
+project.save
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+project = Project.new(
+  title: 'Marin Specialty Care: Corrine Fuller Room',
+  description: 'This Center includes 12 chairs and one bed/private room, offering 16,900 patient visits per year including adults (18 and older) from all backgrounds and ethnic groups. Paint, wallpaper, curtains, stencils, art work, refinish cabinets, etc. This is now a much more hopeful and peaceful environment for those suffering with cancer and needing chemotherapy. Patients and staff alike are appreciative and enjoy the new environments. Below are two of the rooms.',
+  volunteers: 'Designer Corinne Fuller, Taylor Watts, Mario Ballard',
+  location: 'Greenbrae, CA'
+)
+project.images.new().photo_from_url("https://s3-us-west-1.amazonaws.com/rtr4c-dev/projects/Projects/Marin_Corrine_Room/pict_one.jpg")
+project.save
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+project = Project.new(
+  title: 'Marin Specialty Care: Sophia\'s Room',
+  description: 'This Center includes 12 chairs and one bed/private room, offering 16,900 patient visits per year including adults (18 and older) from all backgrounds and ethnic groups. Paint, wallpaper, curtains, stencils, art work, refinish cabinets, etc. This is now a much more hopeful and peaceful environment for those suffering with cancer and needing chemotherapy. Patients and staff alike are appreciative and enjoy the new environments. Below are two of the rooms.',
+  volunteers: 'Designer Kimball Starr, Brian Marshall, Mario Ballard ',
+  location: 'Greenbrae, CA'
+)
+project.images.new().photo_from_url("https://s3-us-west-1.amazonaws.com/rtr4c-dev/projects/Projects/Marin_Sophias_Room/pict_one.jpg")
+project.save
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+project = Project.new(
+  title: 'Oakland Children\'s Hospital',
+  description: '12 rooms including 14 chairs, beds and/or cribs, playroom and staff lounge serving 10,920 patient visits per year. Ten designers and 50+ volunteers contributed to this facility redesign using paint, stencils, decals, wall art, ceiling sky tiles and new privacy curtains. This project wad dedicated in memory of Wendy Heckman, daughter of lead RTR4C designer, Marsha Heckman.',
+  volunteers: 'NA',
+  location: 'Oakland, CA'
+)
+project.images.new().photo_from_url("https://s3-us-west-1.amazonaws.com/rtr4c-dev/projects/Projects/Aria/pict_one.png")
+project.save
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+project = Project.new(
+  title: 'El Camino Hospital',
+  description: 'Five rooms including 4 beds serving 1,300 patient visits per year. One designer and 30+ volunteers using stencils and wall art transformed the environment in memory of Ken Cavanaugh who was treated for cancer at this facility. This project provided Ken’s wife Linda with the opportunity to make these rooms a better place in his honor.',
+  volunteers: 'NA',
+  location: 'Mountain View, CA'
+)
+project.images.new().photo_from_url("https://s3-us-west-1.amazonaws.com/rtr4c-dev/projects/Projects/El_Camino/pict_one.jpg")
+project.save
+
+
+
+
+
