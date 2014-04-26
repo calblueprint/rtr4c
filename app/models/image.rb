798fccf1d32fe80b507f6cdf7aa89f353ca11207
maobster <youj3an@gmail.com>
2014-04-26 14:47:06 -0700

    remove resizing from image.rb; no imagemagick required

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,7 +1,6 @@
 class Image < ActiveRecord::Base
   belongs_to :imageable, polymorphic: true
   has_attached_file :photo, default_url: "/missing.png", styles: {
-    medium: '300x300'
   }
   validates_attachment_content_type :photo, :content_type => /\Aimage\/.*\Z/
 
