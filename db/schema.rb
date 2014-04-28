bad6573878507c924d29e0af45961db74c7e6a92
Hao Guan <haohowardguan@gmail.com>
2014-04-28 03:05:11 -0700

    cart now supports items with size options and purchases exports also reflects this change

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140427234330) do
+ActiveRecord::Schema.define(version: 20140428064723) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 20140427234330) do
     t.integer  "product_id"
     t.integer  "cart_id"
     t.integer  "purchase_id"
+    t.string   "selected_size"
   end
 
   create_table "carts", force: true do |t|
