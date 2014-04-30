e70169845b95342c715e9037f87d584d00c2c503
Hao Guan <haohowardguan@gmail.com>
2014-04-29 19:38:43 -0700

    some quick news updates

--- app/models/press_update.rb
+++ app/models/press_update.rb
@@ -6,4 +6,12 @@ class PressUpdate < ActiveRecord::Base
   def get_embed_link
     self.videolink.split('=').last if self.videolink
   end
+
+  def is_youtube
+    true if self.videolink.include? "youtube.com"
+  end
+
+  def is_embed
+    true if self.videolink.include? "embed"
+  end
 end
