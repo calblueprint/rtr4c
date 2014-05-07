0b6ba9b133c27f730ce8f6b2527d6b8b6e26a435
maobster <youj3an@gmail.com>
2014-05-07 01:03:53 -0700

    create designer model and update projects controller

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,7 @@
 class Project < ActiveRecord::Base
   has_many :images, as: :imageable
   accepts_nested_attributes_for :images, allow_destroy: true
+
+  has_many :designers
+  accepts_nested_attributes_for :designers, allow_destroy: true
 end
