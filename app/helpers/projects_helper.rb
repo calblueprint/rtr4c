adcfe0abb6fc65869c80b0c3cd132a193d57d1ec
maobster <youj3an@gmail.com>
2014-05-07 11:41:11 -0700

    add container and context divs for some forms

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -1,9 +1,9 @@
 module ProjectsHelper
   def designer_url(designer)
     if designer.url.blank?
-      designer.name
+      "#{designer.name}, "
     else
-      link_to designer.name, designer.url
+      "#{link_to designer.name, designer.url}, "
     end
   end
 end
