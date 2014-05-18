b00c0aa194a4cfae5a399191e78534ca38e9d4d9
Hao Guan <haohowardguan@gmail.com>
2014-05-17 23:23:34 -0700

    fixed a lot of checkout stuff for both donations and checkout and email works for donations now

--- app/mailers/store_mailer.rb
+++ app/mailers/store_mailer.rb
@@ -5,5 +5,12 @@ class StoreMailer < ActionMailer::Base
     email = mail_info[:mail]
     @purchase = mail_info[:purchase]
     mail(:to => email, :subject => "Your Checkout Confirmation with RTR4C");
+  end  
+
+  def confirm_donation(mail_info)
+    email = mail_info[:mail]
+    @donor = mail_info[:donor]
+    @donation = mail_info[:donation]
+    mail(:to => email, :subject => "Your Donation with RTR4C");
   end
 end
