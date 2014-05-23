7189817569442f6d5ea57efefcffaa7658d791f7
Kevin Wu <kevintxwu@gmail.com>
2014-05-22 23:28:36 -0700

    EVERYTHINGggg except so much more to do sigh

--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,7 @@ Rtr4c::Application.routes.draw do
   get 'logout', to: 'sessions#destroy', as: 'logout'
   get 'store',  to: 'products#index',   as: 'store'
   get 'contact',to: 'contacts#new',     as: 'contact'
-  get 'contribute/online', to: 'donations#new',     as: 'contribute/online'
+  get 'donate/online', to: 'donations#new',     as: 'donate/online'
   get 'donate', to: 'static#donate', as: 'donate'
 
   post '/add_to_cart/:product_id' => 'cart#add_to_cart', :as => 'add_to_cart'
